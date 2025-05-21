f33c918825a8f3809f0451ba9899d4c959ec3a98
Davito321s <davito101011@gmail.com>
2025-05-21 18:43:50 -0400

    Agregado login de usuarios + panel admin

---
+++ PasaFestWebV2/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PasaFestWebV2.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Panel()
+        {
+            // Validamos si hay un nombre en TempData (o más adelante Session)
+            if (TempData["AdminNombre"] == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+
+            ViewBag.NombreAdmin = TempData["AdminNombre"];
+            return View();
+        }
+    }
+}
